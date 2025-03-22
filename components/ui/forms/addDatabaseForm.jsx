a0f32ed0037ea89f7259cbaa5ef13c995f3c9648
Paweł Żabczyński <p_k_zab@wp.pl>
2025-03-22 02:37:18 +0100

    added addForm  and corresponding components and a template image

---
+++ components/ui/forms/addDatabaseForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Input } from "@/components/ui/input"
+// import { Textarea } from "../ui/textarea";
+// import ImageCarousel from "../ui/imageCarousel";
+import { Textarea } from "../textarea";
+import { CarouselSize } from "../imageCarousel";
+import { Button } from "../button";
+
+export default function AddDatabaseForm(){
+    return (
+    <>
+    <Input placeholder="nazwa"/>
+    <Input placeholder="cena" type="number"/>
+    <Textarea placeholder="opis"/>
+    <div className="juatify-center">
+    <CarouselSize images={["template.png","template.png","template.png"]}></CarouselSize>
+    </div>
+    <Button className="background-color: var(--color-red-300);">Dodaj</Button>
+
+    </>)
+}
