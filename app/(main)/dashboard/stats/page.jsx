a3ec2c4782e2a6dde1491772804ec0a014129862
Dorota Duda <panidora01@gmail.com>
2025-03-22 11:41:17 +0100

    added bar and pipe charts

--- app/(main)/dashboard/stats/page.jsx
+++ app/(main)/dashboard/stats/page.jsx
@@ -1,10 +1,26 @@
+import { BarChartComp } from "@/components/charts/bar-chart";
 import { LineChartComp } from "@/components/charts/line-chart";
+import { PieChartComp } from "@/components/charts/pie-chart";
+
 
 export default function Statistics() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>hello</div>
-      <LineChartComp>hello</LineChartComp>
+    <div className="grid grid-rows-[auto_auto] grid-cols-1 gap-8 min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-5xl">
+        <div className="h-[300px] w-full h-full">
+          <LineChartComp />
+        </div>
+        <div className="h-[300px] w-full h-full">
+          <PieChartComp />
+        </div>
+      </div>
+
+
+      <div className="h-[300px] w-full h-full">
+        <BarChartComp/>
+      </div>
     </div>
   );
 }
+
